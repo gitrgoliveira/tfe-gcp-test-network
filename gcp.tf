949704201d4470b43c56a9f57044eeb5eb3638d2
Ricardo Oliveira <ricardo@hashicorp.com>
2020-08-17 14:33:04 +0100

    Update gcp.tf

--- gcp.tf
+++ gcp.tf
@@ -12,19 +12,19 @@ module "network" {
   project_id   = var.project
   subnets = [
     {
-      subnet_name = "subnet-01"
-      subnet_ip   = "10.10.10.0/24"
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
       subnet_region = var.region
     },
     {
-      subnet_name = "subnet-02"
-      subnet_ip   = "10.10.20.0/24"
+      subnet_name   = "subnet-02"
+      subnet_ip     = "10.10.20.0/24"
       subnet_region = var.region
     },
 
     {
-      subnet_name = "subnet-03"
-      subnet_ip   = "10.10.30.0/24"
+      subnet_name   = "subnet-03"
+      subnet_ip     = "10.10.30.0/24"
       subnet_region = var.region
     }
   ]
